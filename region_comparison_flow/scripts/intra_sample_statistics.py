6b5f21b54b4cfb74029966b74adcde66c2ed0165
Philippa Spangenberg <philippa.spangenberg@arcor.de>
2026-02-23 15:13:23 +0100

    update plots / annotation in intra_sample_statistics.py

--- region_comparison_flow/scripts/intra_sample_statistics.py
+++ region_comparison_flow/scripts/intra_sample_statistics.py
@@ -26,17 +26,17 @@ def plot_sample_percentage(percentage_per_row, fc_thr, sample_perc_thr, region_1
         fc_sample_percentage_df = fc_sample_percentage_df.head(20)  # take the first 20 rows
     print(fc_sample_percentage_df)
 
-    plt.figure(figsize=(7, 10))
+    fig, ax = plt.subplots(figsize=(7, 10))
     sns.barplot(data=fc_sample_percentage_df, x='sample percentage', y='m/z', orient='h',
-                order=fc_sample_percentage_df['m/z'])
+                order=fc_sample_percentage_df['m/z'], ax=ax)
     # sns.barplot(data=fc_sample_percentage_df, x='sample percentage', y='m/z', orient='h',
     #             order=fc_sample_percentage_df['sample percentage'])
     if high:
         plt.title('sample percentage of m/z with log2(FC) > {0} ({1} vs {2})'.format(fc_thr, region_1, region_2))
     else:
         plt.title('sample percentage of m/z with log2(FC) < -{0} ({1} vs {2})'.format(fc_thr, region_1, region_2))
-    plt.xlabel('samples (%)')
-    plt.ylabel('m/z')
+    ax.set_xlabel('samples (%)')
+    ax.set_ylabel('m/z')
     if high:
         fl_name = '{}_fc_{}_sample_percentage'.format(fc_thr, sample_perc_thr)
     else:
@@ -45,24 +45,26 @@ def plot_sample_percentage(percentage_per_row, fc_thr, sample_perc_thr, region_1
     plt.savefig(os.path.join(args.output_dir, fl_name + '.svg'))
 
     if plot:
-        plt.show()
-    plt.close()
+        fig.show()
+    plt.close(fig)
 
 
-def mean_fc_bar_plot(df, n, output_file, annot_fl='', plot=False):
-    y_col='index'
-
+def mean_fc_bar_plot(df, n, output_file, output_csv, annot_fl='', plot=False):
     if annot_fl != '':
-        annot_df = pd.read_csv(annot_fl, delimiter=';', index_col=0)
-        annot_df.index = annot_df.index.round(4)
-        df.index = df.index.round(4)
+        annot_df = pd.read_csv(annot_fl, delimiter=',', index_col=0)
 
-        df = annot_df.merge(df, how='inner', left_index=True, right_index=True)
-        print(df)
+        annot_df.index = annot_df.index.astype(float).round(4)
+        df.index = df.index.astype(float).round(4)
 
-        y_col = 'lipid'
+        common = df.index.intersection(annot_df.index)
+        df_join = df.loc[common].join(annot_df.loc[common])
 
-    top_rows = df.nlargest(n, 'row_mean_above_thresh').reset_index()
+        y_col = 'Name'
+        top_rows = df_join.nlargest(n, 'row_mean_above_thresh').reset_index()
+
+    else:
+        y_col = 'index'
+        top_rows = df.nlargest(n, 'row_mean_above_thresh').reset_index()
 
     if annot_fl == '':
         top_rows['index'] = top_rows['index'].astype(str)
@@ -70,23 +72,40 @@ def mean_fc_bar_plot(df, n, output_file, annot_fl='', plot=False):
         top_rows.drop(columns=['index'], inplace=True)
 
     top_rows = top_rows.sort_values('row_mean_above_thresh', ascending=False)
-    # print("top_rows=", top_rows)
+    top_rows.to_csv(output_csv)
+    print("top_rows=", top_rows)
 
     # seaborn bar plot
-    plt.figure(figsize=(7, 10))
-    sns.barplot(y=y_col, x='row_mean_above_thresh', data=top_rows)
-    plt.ylabel('m/z')
-    plt.xlabel('mean log2(FC)')
+    fig, ax = plt.subplots(figsize=(7, 10))
+    sns.barplot(y=y_col, x='row_mean_above_thresh', data=top_rows, ax=ax)
+    ax.set_ylabel('m/z')
+    ax.set_xlabel('mean log2(FC)')
     plt.subplots_adjust(left=0.25)  # Increase left margin
     plt.savefig(output_file)
 
     if plot:
-        plt.show()
-        plt.close()
+        fig.show()
+    plt.close(fig)
+
+
+def plot_best_fc_sample(df, x_col, smpl, regions, out_dir):
+    fig, ax = plt.subplots(figsize=(7, 10))
+    sns.barplot(x=x_col, y=df.index, data=df, orient='h', order=df.index.to_list(), ax=ax)
+    ax.set_xlabel('log2(FC)')
+    ax.set_ylabel('m/z')
+    plt.title('{0} {1} vs {2}'.format(smpl, regions[0], regions[1]))
+
+    plt.savefig(os.path.join(out_dir, 'FC_{0}_{1}_vs_{2}.svg'.format(smpl, regions[0], regions[1])),
+                transparent=True)
+
+    if args.plot:
+        fig.show()
+    plt.close(fig)
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Calculates fold changes between two regions per individual MSI sample')
+    parser = argparse.ArgumentParser(
+        description='Calculates fold changes between two regions per individual MSI sample')
     parser.add_argument('input_file', type=str, help='input file with summarized intensities per region')
     parser.add_argument('output_dir', type=str, help='output directory')
     parser.add_argument('-plot', type=bool, default=False, help='set to True to plot volcano')
@@ -136,19 +155,7 @@ if __name__ == '__main__':
         filtered_smpl_df.to_csv(os.path.join(args.output_dir, res_col + '.csv'))
 
         # plot best FC of sample
-        plt.figure(figsize=(7, 10))
-        sns.barplot(x=res_col, y=filtered_smpl_df.index, data=filtered_smpl_df, orient='h', order=filtered_smpl_df.index.to_list())
-        plt.xlabel('log2(FC)')
-        plt.ylabel('m/z')
-        plt.title('{0} {1} vs {2}'.format(smpl, regions[0], regions[1]))
-
-        plt.savefig(os.path.join(args.output_dir, 'FC_{0}_{1}_vs_{2}.svg'.format(smpl, regions[0], regions[1])),
-                    transparent=True)
-
-        if args.plot:
-            plt.show()
-
-        plt.close()
+        plot_best_fc_sample(df=filtered_smpl_df, x_col=res_col, smpl=smpl, regions=regions, out_dir=args.output_dir)
 
     print(df)
     df.to_csv(os.path.join(args.output_dir, 'FC.csv'))
@@ -160,7 +167,8 @@ if __name__ == '__main__':
     print(fc_df)
 
     threshold = args.sample_perc_thr * fc_df.shape[1]  # sample percentage of the number of columns
-    filtered_df = fc_df[(fc_df.gt(args.fc_thr).sum(axis=1) >= threshold) | (fc_df.lt(-args.fc_thr).sum(axis=1) >= threshold)]
+    filtered_df = fc_df[
+        (fc_df.gt(args.fc_thr).sum(axis=1) >= threshold) | (fc_df.lt(-args.fc_thr).sum(axis=1) >= threshold)]
 
     filtered_high_df = fc_df[(fc_df.gt(args.fc_thr).sum(axis=1) >= threshold)]
     filtered_low_df = fc_df[(fc_df.lt(-args.fc_thr).sum(axis=1) >= threshold)]
@@ -179,12 +187,37 @@ if __name__ == '__main__':
     percentage_per_row_low = (filtered_low_df < -args.fc_thr).sum(axis=1) / filtered_df.shape[1] * 100
 
     # plot
-    plot_sample_percentage(percentage_per_row_high, args.fc_thr, args.sample_perc_thr, regions[0], regions[1], True, args.plot)
-    plot_sample_percentage(percentage_per_row_low, args.fc_thr, args.sample_perc_thr, regions[0], regions[1], False, args.plot)
+    plot_sample_percentage(percentage_per_row_high, args.fc_thr, args.sample_perc_thr, regions[0], regions[1], True,
+                           args.plot)
+    plot_sample_percentage(percentage_per_row_low, args.fc_thr, args.sample_perc_thr, regions[0], regions[1], False,
+                           args.plot)
 
     # save filtered df
-    filtered_high_df.to_csv(os.path.join(args.output_dir, '{}_fc_{}_samples_filtered.csv'.format(args.fc_thr, args.sample_perc_thr)))
-    filtered_low_df.to_csv(os.path.join(args.output_dir, '-{}_fc_{}_samples_filtered.csv'.format(args.fc_thr, args.sample_perc_thr)))
+    filtered_high_df.to_csv(
+        os.path.join(args.output_dir, '{}_fc_{}_samples_filtered.csv'.format(args.fc_thr, args.sample_perc_thr)))
+    filtered_low_df.to_csv(
+        os.path.join(args.output_dir, '-{}_fc_{}_samples_filtered.csv'.format(args.fc_thr, args.sample_perc_thr)))
+
+    # save filtered df with annotation
+    if args.annotations_file != '':
+        annot_df = pd.read_csv(args.annotations_file, delimiter=',', index_col=0)
+
+        annot_df.index = annot_df.index.astype(float).round(4)
+        filtered_high_df.index = filtered_high_df.index.astype(float).round(4)
+        filtered_low_df.index = filtered_low_df.index.astype(float).round(4)
+
+        common_high = filtered_high_df.index.intersection(annot_df.index)
+        common_low = filtered_low_df.index.intersection(annot_df.index)
+
+        filtered_high_df_join = filtered_high_df.loc[common_high].join(annot_df.loc[common_high])
+        filtered_low_df_join = filtered_low_df.loc[common_low].join(annot_df.loc[common_low])
+
+        filtered_high_df_join.to_csv(os.path.join(args.output_dir,
+                                                  '{}_fc_{}_samples_filtered_annot.csv'.format(args.fc_thr,
+                                                                                               args.sample_perc_thr)))
+        filtered_low_df_join.to_csv(os.path.join(args.output_dir,
+                                                 '-{}_fc_{}_samples_filtered_annot.csv'.format(args.fc_thr,
+                                                                                               args.sample_perc_thr)))
 
     # calculate mean fc of samples with fc above threshold
     filtered_high_df['row_mean_above_thresh'] = filtered_high_df.where(filtered_high_df > args.fc_thr).mean(axis=1)
@@ -199,12 +232,16 @@ if __name__ == '__main__':
                      n=top_n,
                      output_file=os.path.join(args.output_dir, '{}_fc_{}_samples_top_mean_FC.svg'.format(args.fc_thr,
                                                                                                          args.sample_perc_thr)),
+                     output_csv=os.path.join(args.output_dir, '{}_fc_{}_samples_top_mean_FC.csv'.format(args.fc_thr,
+                                                                                                        args.sample_perc_thr)),
                      annot_fl=args.annotations_file,
                      plot=args.plot)
     mean_fc_bar_plot(df=filtered_low_df,
                      n=top_n,
                      output_file=os.path.join(args.output_dir, '-{}_fc_{}_samples_top_mean_FC.svg'.format(args.fc_thr,
                                                                                                           args.sample_perc_thr)),
+                     output_csv=os.path.join(args.output_dir, '-{}_fc_{}_samples_top_mean_FC.csv'.format(args.fc_thr,
+                                                                                                         args.sample_perc_thr)),
                      annot_fl=args.annotations_file,
                      plot=args.plot)
 
